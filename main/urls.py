dff4d81e3a227db4655ee50b159d54ef014f7315
Andy Tran <ajtran@umich.edu>
2015-08-02 16:53:46 -0400

    'have google places api set up and return json to results page'

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, patterns
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.find, name='find'),
+    url(r'^$', views.index, name='index'),
     url(r'^results/$', views.find, name='find'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from lxml import html
+import requests
+import json
+import argparse
+import json
+import pprint
+import sys
+import urllib
+import urllib2
+
+import oauth2
+
+
+GOOGKEY = '&key=AIzaSyBmu-0gJXE0CMNQRYnXeLCM_oN2_WXSR7E'
+SEARCHURL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='
+KEYWORDS = 'skate+shop+'
 
 # Create your views here.
 def index(request):
 	return render(request, 'main/index.html')
+def find(request):
+	zcode = request.GET['zip']
+	search = SEARCHURL + KEYWORDS + zcode + GOOGKEY
+	page =  urllib.urlopen(search)
+	jsonRaw = page.read()
+	data = json.loads(jsonRaw)
+	return render(request, 'main/results.html', {'data' : data, 'search' : search})
